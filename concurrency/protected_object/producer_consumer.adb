3cfdbccc2a78aa5b14ffd28e7350b90eb449a639
xyderos <kostas@xyderos.com>
2023-04-02 21:26:25 +0200

    added protected object

---
+++ concurrency/protected_object/producer_consumer.adb
@@ -0,0 +1,87 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+with Ada.Real_Time;
+use Ada.Real_Time;
+
+with Buffer;
+use Buffer;
+
+with Ada.Numerics.Discrete_Random;
+
+procedure ProducerConsumer_Prot is
+
+   N : constant Integer := 10; -- Number of produced and consumed tokens per task
+	
+   X : constant Integer := 3; -- Number of producers and consumers
+	
+   -- Random Delays
+   subtype Delay_Interval is Integer range 50..250;
+  
+   package Random_Delay is new Ada.Numerics.Discrete_Random (Delay_Interval);
+  
+   use Random_Delay;
+  
+   G : Generator;
+
+   cBuffer : circularBuffer;
+
+   task type Producer;
+
+   task type Consumer;
+
+   task body Producer is
+  
+      Next : Time;
+  
+   begin
+   
+      Next := Clock;
+   
+      for I in 1..N loop
+
+         Put_Line("Adding " & Integer'Image(I) & " to the buffer");
+
+         cBuffer.Put(I);
+
+         Next := Next + Milliseconds(Random(G));
+       
+         delay until Next;
+     
+      end loop;
+  
+   end;
+
+   task body Consumer is
+   
+      Next : Time;
+   
+      X : Integer;
+  
+   begin
+    
+      Next := Clock;
+     
+      for I in 1..N loop
+
+         cBuffer.Get(X);
+			
+         Put_Line("Reading from buffer the value: " & Integer'Image(X));
+        
+         Next := Next + Milliseconds(Random(G));
+         
+         delay until Next;
+      
+      end loop;
+   
+   end;
+	
+	P: array (Integer range 1..X) of Producer;
+	
+   C: array (Integer range 1..X) of Consumer;
+	
+begin -- main task
+  
+   null;
+
+end ProducerConsumer_Prot;
